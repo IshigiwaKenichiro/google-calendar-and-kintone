7c7e5c232fef7fd3b4a2d4ed35e7a8d4b9d543f8
IshigiwaKenichiro <ishigiwa.wk@gmail.com>
2024-04-26 16:09:58 +0900

    ReadMeとソースのコメントを調整

--- 0.env.js
+++ 0.env.js
@@ -1,11 +1,26 @@
 const env = {
+    //GoogleカレンダーのIDです。
     calId: '',
+
+    //ログを出力するスプレッドシートのURLです。
     logBookUrl: 'https://docs.google.com/spreadsheets/d/***/',
+    
+    //kintoneのアクセス情報です。
     kintone: {
+        
+        //cybozu.comのURLです。
         baseUrl: "https://***.cybozu.com",
+
+        //データの登録・更新・削除権限が必要なAPIキーです。
         apiToken: "***",
+        
+        //アプリID
         appId: '178',
+
+        //ベーシック認証のユーザー名です。ない場合は空文字。
         basicUsername: "",
+        
+        //ベーシック認証のパスワードです。ない場合は空文字。
         basicPassword: ""
     },
 }
--- ReadMe.md
+++ ReadMe.md
@@ -1,10 +1,32 @@
 # Google App Script & kintone
 GoogleAppScriptとkintoneのコラボレーション構築例です。
+claspを利用します。
 
-# install
+https://github.com/google/clasp
+
+
+# installのしかた
+## パッケージインストール
 ```
 npm i 
 ```
 
-# 環境
-0.main.jsの変数を適切に変えてください。
+## ソースの環境変数
+0.env.jsの変数を適切に変えてください。
+
+
+## kintoneアプリ
+/kintoneにアプリテンプレートがあります。ご利用ください。
+GoogleCalendarのプロパティとkintoneアプリのフィールドのマッピングは
+1.converters.jsを見てください。
+
+# コマンドの説明
+## clone
+既存のAppScriptプロジェクトとリンクします。
+
+## push
+リンクされたプロジェクトからソースを同期してローカルへ上書きします。
+
+## pull
+リンクされたプロジェクトへソースをアップロードして同期します。
+
